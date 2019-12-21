e9eaa099d748f6cf825eb8011e16edf2f664d43d
Robert Glyde <robert.glyde@sky.uk>
2019-12-21 16:37:31 +0000

    stopped ignoring project directory

---
+++ project/DockerSettings.scala
@@ -0,0 +1,19 @@
+import com.typesafe.sbt.SbtNativePackager.autoImport.packageName
+import com.typesafe.sbt.packager.docker.Cmd
+import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
+import sbt.Keys._
+
+object DockerSettings {
+
+  lazy val common = Seq(
+    dockerBaseImage := "openjdk:8u201-alpine",
+    dockerRepository := Some("glyderj"),
+    dockerLabels := Map("maintainer" -> "r-glyde"),
+    dockerUpdateLatest := true,
+    packageName in Docker := s"kafka-siren",
+    dockerCommands ++= Seq(
+      Cmd("USER", "root"),
+      Cmd("RUN", "apk update && apk add bash")
+    )
+  )
+}
